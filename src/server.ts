05bc61596206c59319e4276e15c5b99b9482d928
Vu Hoang <hoangvv@icts.vn>
2021-03-04 14:10:07 +0700

    i3

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ const app = express();
 
 const PORT = 8000;
 
-app.get('/', (req, res) => res.send('The sailor Nodejs 2'));
+app.get('/', (req, res) => res.send('The sailor Nodejs 3'));
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
